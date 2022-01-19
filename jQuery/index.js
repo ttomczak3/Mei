af01abfd3a8b993a1f5740ea28029bd1d64b0648
ttomczak3 <78294042+ttomczak3@users.noreply.github.com>
2022-01-18 21:29:06 -0600

    Add files via upload

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,22 @@
-$(".toggle-button").click(function(){
-    $(this).toggleClass("click");
-    $(".navbar-link").toggleClass("active");
+$(document).ready(function(){
+
+    // Mobile nav menu
+    $(".toggle-button").click(function(){
+
+        $(this).toggleClass("fa-times");
+        $(".navbar-link").toggleClass("active");
+
+    });
+
+    // Avatar image transition
+    $('.avatar-image').hover(function(){
+        
+        $(this).attr("src", "images/index/avatar1.png");
+        
+    }, function(){
+
+        $(this).attr("src", "images/index/avatar.png");
+        
+    })
+
 });
